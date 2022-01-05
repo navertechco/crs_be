3f567cffa63692715e51b4f49c9a06a609751690
José Cuevas NL <naver.developer@gmail.com>
2022-01-05 13:53:45 -0500

    stable table catalog

--- service/src/BUSINESS/System/FindCatalog/BSFindCatalog/__init__.py
+++ service/src/BUSINESS/System/FindCatalog/BSFindCatalog/__init__.py
@@ -3,6 +3,7 @@ try:
 except ImportError:
     __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 from ..DSFindCatalog import DSFindCatalog
+from ..DSFindTableCatalog import DSFindTableCatalog
 import logging
 from naver_core import *
 from src.BUSINESS.Dto import CatalogDetailDto
@@ -19,7 +20,20 @@ def BSFindCatalog(input):
                 catalogDto = CatalogDetailDto(result)
                 if catalogDto.catalog==catalog:
                     catalog_list["catalogs"][catalog].append(catalogDto.__dict__())
-                    
+        
+        
+        tables = getValue(input, 'tables')
+        if tables is not None:
+            for table in tables:
+                name = table.get('name')
+                result = DSFindTableCatalog(table)
+                # catalogDto = CatalogDetailDto(result)
+                catalog_list["catalogs"][name]=(result)
+            pass
+            
+        
+        
+                   
         return catalog_list
 
     except Exception as e:

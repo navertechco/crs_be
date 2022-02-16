b6ec1c525c5327d5fcd471984da691eb3aa58bd2
José Cuevas NL <naver.developer@gmail.com>
2022-02-16 09:25:21 -0500

    fix new ds

--- service/src/BUSINESS/System/LogProspect/DSLogProspect/__init__.py
+++ service/src/BUSINESS/System/LogProspect/DSLogProspect/__init__.py
@@ -2,14 +2,21 @@ try:
     __import__('pkg_resources').declare_namespace(__name__)
 except ImportError:
     __path__ = __import__('pkgutil').extend_path(__path__, __name__)
-# from ..Core import Business
+from src.INFRA.WEB.App.routes import app
+from naver_db import NaverDB
+from naver_config import NaverConfig
 from naver_core import *
 
-def DSLogProspect(udata):
+config = NaverConfig(app)
+nbd = NaverDB(app, config)
+
+def DSLogProspect(input):
     try:
-        business = Business(mySession)
-        result = business.method(udata)
-        return result
+        table = "TOUR"
+        schema = "entities"
+        stm = "SELECT 1" 
+        res = nbd.persistence.getQuery(stm, table)
+        return res
 
     except Exception as e:
         raise e

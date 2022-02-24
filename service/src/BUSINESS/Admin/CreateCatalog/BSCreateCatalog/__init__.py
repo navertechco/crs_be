ce7adb728be637f4272ccf89911fae73036aa40c
José Cuevas NL <naver.developer@gmail.com>
2022-02-24 17:11:38 -0500

    fix FS savetour

--- service/src/BUSINESS/Admin/CreateCatalog/BSCreateCatalog/__init__.py
+++ service/src/BUSINESS/Admin/CreateCatalog/BSCreateCatalog/__init__.py
@@ -4,20 +4,11 @@ except ImportError:
     __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 from ..DSCreateCatalog import DSCreateCatalog
 from naver_core import *
-import ast, json,re, yaml
-def myconverter(o):
-    char = str(o)
-    return char
+
     
 def BSCreateCatalog(input):
     try:
-        state = input.get('state')
-        if state == "tour":
-            value = getValue(input, 'value')  
-            value = yaml.safe_load(value)
-            value = json.dumps(value, default=myconverter)
-            value = json.loads(value)
-            input["data"]["value"] = value
+        
         result = DSCreateCatalog(input)
         if len(result) > 0:
             result["session"].commit()
--- service/src/BUSINESS/Admin/CreateCatalog/FSCreateCatalog/__init__.py
+++ service/src/BUSINESS/Admin/CreateCatalog/FSCreateCatalog/__init__.py
@@ -4,9 +4,19 @@ except ImportError:
     __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 from ..BSCreateCatalog import BSCreateCatalog
 from naver_core import *
-
+import ast, json,re, yaml
+def myconverter(o):
+    char = str(o)
+    return char
 def FSCreateCatalog(input):
     try:
+        state = input.get('state')
+        if state == "tour":
+            value = getValue(input, 'value')  
+            value = yaml.safe_load(value)
+            value = json.dumps(value, default=myconverter)
+            value = json.loads(value)
+            input["data"]["value"] = value
         result = BSCreateCatalog(input)
         return Ok(result)
 

fc5952155b4989ba332e4cc2b87d41816baa4ebb
José Cuevas NL <naver.developer@gmail.com>
2022-05-11 11:57:20 -0500

    secure

--- service/src/INFRA/WEB/App/utils.py
+++ service/src/INFRA/WEB/App/utils.py
@@ -6,7 +6,7 @@ def removeBytePrefix(value):
     """Method to remove the byte prefix from the response
     Args:
         value (value): value to be decorated
-        
+
     Returns:
         value: Replaced value
     """
@@ -23,6 +23,19 @@ def decryptdata():
     jsondata = ast.literal_eval(jsonConvert(res))
     data = {"data": jsondata}
     return data
+
+
+def secure_method(func):
+    def inner(*args, **kwargs):
+        token = request.headers.get('token')
+        if token is None:
+            return ErrorResponse(MAINTENANCE)
+        if token != TOKEN:
+            return ErrorResponse(MAINTENANCE)
+        returned_value = func(*args, **kwargs)
+        return returned_value
+    inner.__name__ = func.__name__
+    return inner
 
 
 def encrypted(function):
